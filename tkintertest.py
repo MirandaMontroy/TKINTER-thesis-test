c06b91818f25a47f9100d81904d0453fd2b16950
Miranda Montroy <Miranda_Montroy1@baylor.edu>
2021-02-09 22:41:14 -0600

    first window written

--- tkintertest.py
+++ tkintertest.py
@@ -1,3 +1,18 @@
 from tkinter import *
 
-root = Tk()
+#makes a window
+root=Tk()
+#puts text in the window 
+myLabel = Label(root, text="This was pretty easy")
+
+#titles the window 
+root.title("Thesis time baby")
+
+#file has to be .ico for this to work 
+#root.iconbitmap(r'C:\Users\mmboo\Downloads\naturegirl.png')
+
+#makes the text appear in window
+myLabel.pack()
+
+#prevents window from closing 
+root.mainloop()
